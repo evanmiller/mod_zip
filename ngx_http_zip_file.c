5c859a3d963f9e79a651abaa428c0c766c103127
nanaya <me@myconan.net>
2016-05-30 15:06:21 +0900

    Fix syntax error when no iconv is configured

--- ngx_http_zip_file.c
+++ ngx_http_zip_file.c
@@ -327,8 +327,9 @@ ngx_http_zip_generate_pieces(ngx_http_request_t *r, ngx_http_zip_ctx_t *ctx)
 
                 file->filename_utf8_crc32 = ngx_crc32_long(file->filename_utf8.data, file->filename_utf8.len);
             }
+            else
 #endif
-              else if(vv->len) {
+              if(vv->len) {
                 const char * sep = ngx_http_zip_strnrstr((const char*)file->filename.data, file->filename.len,
                                                          (const char*)vv->data, vv->len);
                 if(sep) {
